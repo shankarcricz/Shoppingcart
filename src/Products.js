cb87fe4103d73eb6d9d5ffff5a7c8f7bf99e1879
shankarcricz <shankarcricz@gmail.com>
2022-12-22 22:39:09 +0530

    Card styling for product lists

--- src/Products.js
+++ src/Products.js
@@ -4,39 +4,55 @@ import { fetchProducts } from './actions';
 import { connect } from 'react-redux';
 
 const Products = (props) => {
-    useEffect(()=>{
-        props.fetchProducts(); 
-    }, [])   
+    useEffect(() => {
+        props.fetchProducts();
+    }, [])
     console.log(props.products)
-    
+
     const renderList = () => {
         return (
             props.products && props.products.map((product) => {
                 return (
-                    <div className='col style'>
-                        <div key={product.id} className='card'>
-                        <div>{product.title}</div>
+                    <div className='col mb-5 style' key={product.id}>
+                        
+                        <div class="ui card">
+                            <div class="content">
+                                <div class="right floated meta">{product.title}</div>
+                                <img class="ui avatar image" src={product.image} /> {product.description}
+                            </div>
+                            <div class="content">
+                                <span class="right floated">
+                                    <i class="heart outline like icon"></i>
+                                    {product.rating.rate}
+                                </span>
+                                <i class="comment icon"></i>
+                                {product.rating.count} counts
+                            </div>
+                            
                         </div>
-                    </div>
                     
+                        
+                    </div>
+
+
                 );
             })
         );
-        
+
     }
-    
+
     return (
-        <div className='row'>
+        <div className='row mt-5'>
             {renderList()}
         </div>
     );
 }
 
-const mapStateToProps = ({Products}) => { 
+const mapStateToProps = ({ Products }) => {
     return {
-        products : Products[0]
+        products: Products[0]
     };
 }
 export default connect(mapStateToProps, {
-    fetchProducts : fetchProducts
+    fetchProducts: fetchProducts
 })(Products);
