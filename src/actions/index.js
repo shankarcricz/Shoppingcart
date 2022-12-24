4d34c81590f0a43c66316f0da522606c204d91d6
shankarcricz <shankarcricz@gmail.com>
2022-12-24 16:31:21 +0530

    Redux clean up

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,6 +5,7 @@ const URL =  'https://fakestoreapi.com/products';
 
 export const fetchProducts = () => async (dispatch, getState) => {
     const response = await axios.get(URL);
+    
     dispatch({
         type : 'FETCH_PRODS',
         payload : response.data 
@@ -12,9 +13,16 @@ export const fetchProducts = () => async (dispatch, getState) => {
 }
 
 export const fetchProd = (id) => async (dispatch) => {
-    const response = await axios.get(URL + `/${id}`);
+    const response = await axios.get(URL + `/`+id);
     dispatch({
         type : 'FETCH_PROD',
         payload : response.data
+    })
+}
+
+
+export const removeProd = () => (dispatch) => {
+    dispatch({
+        type:'REMOVE_PROD',
     })
 }
